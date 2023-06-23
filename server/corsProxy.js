6d5c5f95c14de7ae84dba45a23035d5195360626
Yangjia (Yang) Deng <118300314+yang530@users.noreply.github.com>
2023-06-23 18:34:47 -0400

    Add files via upload

--- server/corsProxy.js
+++ server/corsProxy.js
@@ -5,7 +5,7 @@ const path = require("path");
 const app = express();
 app.use(express.json());
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 // Add headers before the routes are defined
 app.use(function (req, res, next) {
@@ -44,6 +44,7 @@ app.get("/getWeather/:lat/:lon", (req, res)=>{
     let reqURL = `https://api.open-meteo.com/v1/forecast?latitude=${req.params.lat}&longitude=${req.params.lon}&daily=weathercode,temperature_2m_max,temperature_2m_min&current_weather=true&timezone=auto`
     
     httpReq(reqURL, (error, response, body)=>{
+        //if there is error, bounce error back to front end;
         if(error){
             res.send(error);
         }
